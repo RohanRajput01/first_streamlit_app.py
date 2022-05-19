a5ab12534c3ba57f0350decf7d882d92920f40c2
RohanRajput01 <105836559+RohanRajput01@users.noreply.github.com>
2022-05-19 10:38:33 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -7,13 +7,5 @@ git remote add origin https://github.com/RohanRajput01/first_streamlit_app.py.gi
 git push -u origin main
 
 
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-
-import pandas 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import streamlit 
+streamlit title('my parents new healthy dinner')

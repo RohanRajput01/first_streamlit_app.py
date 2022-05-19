581552a150c22562368ae2ed1776a36e5e956db9
RohanRajput01 <105836559+RohanRajput01@users.noreply.github.com>
2022-05-19 10:20:30 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -5,3 +5,10 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/RohanRajput01/first_streamlit_app.py.git
 git push -u origin main
+
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
